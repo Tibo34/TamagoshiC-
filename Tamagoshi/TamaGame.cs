0a0e141b30437e9d74bb901c99624864d783fafd
Thibaut Molina <thibautmolina@yahoo.fr>
2018-10-11 23:35:02 +0200

    2°

---
+++ Tamagoshi/TamaGame.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+using Tamagoshi.tamagoshi;
+using Tamagoshi.Util;
+
+namespace Tamagoshi.jeu
+{
+    class TamaGame
+    {
+        private ArrayList listDepart;
+        private ArrayList listInLife;
+
+        public TamaGame()
+        {
+            this.listInLife = new ArrayList();
+            this.listDepart = new ArrayList();
+            initialisation();
+        }
+
+        public TamaGame(String[] name)
+        {
+            this.listInLife = new ArrayList();
+            this.listDepart = new ArrayList();
+            initialisation(name);
+        }
+
+        
+
+        private void Addlist(String name)
+        {
+            Tamagoshis tam = new Tamagoshis(name);
+            listDepart.Add(tam);
+            listInLife.Add(tam);
+            
+        }
+
+        private void initialisation(String[] name)
+        {
+            foreach(String n in name)
+            {
+                Addlist(n);
+            }
+        }
+
+        private void initialisation()
+        {
+            String str = "";
+            while (!str.Equals("Fin"))
+            {
+                Utilisateur.afficheEcran("Saisir un nom pour le Tamagoshi : ");
+                str = Utilisateur.saisirClavier();
+                if (!str.Equals("Fin"))
+                {
+                    Addlist(str);
+                }
+
+            }
+        }
+
+        public void play()
+        {
+            int choix = 0;
+            while (!isEnd())
+            {
+                parleAll(listInLife);
+                Utilisateur.afficheEcran("Qui voulez-vous nourrir?");
+                choix = PlayerChoice();
+                Tamagoshis tam =(Tamagoshis) listInLife[choix];
+                tam.mange();
+                consommeEnergieAll(listInLife);
+            }
+        }
+
+        private int PlayerChoice() {
+
+            int choix = -1;
+            String str = "";
+            while (choix < 0)
+            {
+                str = Utilisateur.saisirClavier();
+                try
+                {
+                    choix = int.Parse(str);
+                }
+                catch (FormatException)
+                {
+                    choix = -1;
+                    Utilisateur.afficheEcran("ce n'est pas un nombre !!!");
+                }
+                if (choix > listInLife.Count) {
+                    choix = -1;
+                    Utilisateur.afficheEcran("Ce tamagoshi n'existe pas !!");                    
+                }
+            }
+            return choix;
+        }
+
+        private void parleAll(ArrayList tam)
+        {
+            int i = 0;
+            foreach(Tamagoshis t in tam)
+            {
+                t.parle(i);
+                i++;
+               
+            }
+        }
+
+        private void consommeEnergieAll(ArrayList tam)
+        {
+            ArrayList remove = new ArrayList();
+            foreach(Tamagoshis t in tam)
+            {
+                t.consommeEnergie();
+                if (t.getEnergy()<=0) {
+                    remove.Add(t);
+                }
+            }
+            foreach(Tamagoshis t in remove)
+            {
+                tam.Remove(t);
+            }
+        }
+
+        public Boolean isEnd()
+        {
+            return listInLife.Count<=0;
+             
+        }
+
+
+    }
+}
---
+++ Tamagoshi/Tamagoshi.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tamagoshi.Util;
+
+namespace Tamagoshi.tamagoshi
+{  
+        class Tamagoshis
+        {
+
+            private String name;
+            private int age;
+            private int energy;
+            private int maxEnergy;
+            private static int lifetime = 10;
+            private Random rand;
+
+            public Tamagoshis(String name)
+            {
+                this.name = name;
+                this.age = 0;
+                this.rand = new Random();
+                this.energy = rand.Next(3, 7);
+                this.maxEnergy = rand.Next(5, 9);
+            }
+
+            public Boolean parle()
+            {
+                if (energy > 4)
+                {
+                    Console.WriteLine(name + " est hereux ");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine(name + " est affamé");
+                    return false;
+                }
+            }
+            public Boolean parle(int n)
+            {
+                if (energy > 4)
+                {
+                    Console.WriteLine(name + " est hereux "+n);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine(name + " est affamé "+n);
+                    return false;
+                }
+            }
+
+        public Boolean mange()
+            {
+                if (energy < maxEnergy)
+                {
+                    energy += rand.Next(1, 3);
+                    if (energy > maxEnergy)
+                    {
+                        energy = maxEnergy;
+                    }
+                    Console.WriteLine(name + " est content il a bien mangé");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine(name + " n'a pas faim!");
+                    return false;
+                }
+            }
+
+            public Boolean consommeEnergie()
+            {
+                energy--;
+                if (energy <= 0)
+                {
+                    Console.WriteLine(name + " est KO !");
+                    return false;
+                }
+                return true;
+            }
+
+            public String toString()
+            {
+                return "nom du tamagoshi: " + this.name + " age : " + this.age + " energie :" + energy + " energie max :  " + maxEnergy;
+            }
+
+            public int getEnergy()
+            {
+                return energy;
+            }
+
+           
+        }
+    
+ }
+
