7b53cd403603360e0505620966497d1cfac5b54e
Thibaut Molina <thibautmolina@yahoo.fr>
2018-10-11 23:33:20 +0200

    Jeu Tamagoshi

---
+++ Tamagoshi/Play.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tamagoshi.jeu;
+
+namespace Tamagoshi
+{
+    class Play
+    {
+        public static void Main(String[] args)
+        {
+            TamaGame game = new TamaGame();
+            game.play();
+        }
+    }
+}
